f1b493e10ffbf670fa35ab109f243b9387cf02c7
alan <alan@dssd.com>
2017-09-17 22:35:45 -0700

    Some general improvements and a pre-test to check a puzzle
    Imporve ascii art for print_piece
    piece_has() function to look for bird/side combo
    piece_match_piece() function to compare two pieces
    check_permutation() function to make a first pass at a board
    Made a proper main() function and a brute_force() function.

--- puz.py
+++ puz.py
@@ -61,13 +61,18 @@ pieces = {
        "down": {"bird":2, "side":1},
        "left": {"bird":4, "side":2}},
 }
-def print_piece(num, piece):
+def print_piece(piece):
     """ Function to print out a piece """
-    print("   {0}:{1}".format(piece["up"]["bird"], piece["up"]["side"]))
+
+    print("+--{0}:{1}--+".format(piece["up"]["bird"], piece["up"]["side"]))
+    print "|       |"
     print("{0}:{1} {2} {3}:{4}".format(piece["left"]["bird"], \
       piece["left"]["side"], piece["name"], piece["right"]["bird"],
       piece["right"]["side"]))
-    print("   {0}:{1}".format(piece["down"]["bird"], piece["down"]["side"]))
+    print "|       |"
+    print("+--{0}:{1}--+".format(piece["down"]["bird"], \
+      piece["down"]["side"]))
+
 
 
 def rotate_left(piece, pieces):
@@ -122,11 +127,144 @@ def print_board(board):
     print_board_row(board["bottom"])
 
 
+def piece_has(cp, bs):
+    """ Walk the four sides of the "compare piece" cp, and see if any of
+        the sides match our "board side" bs.  """
+    if cp["up"]["bird"] == bs["bird"] and cp["up"]["side"] != bs["side"]:
+        return True
 
+    if cp["right"]["bird"] == bs["bird"] and cp["right"]["side"] != bs["side"]:
+        return True
+
+    if cp["left"]["bird"] == bs["bird"] and cp["left"]["side"] != bs["side"]:
+        return True
+
+    if cp["down"]["bird"] == bs["bird"] and cp["down"]["side"] != bs["side"]:
+        return True
+
+    return False
+
+
+def piece_match_piece(cp, mp):
+    """ Take mp and walk all four sides of it, looking to see if any of them
+        have a match in cp.  One match is all we need, so return True as soon
+        as we find it. """
+
+    if piece_has(cp, mp["left"]):
+        return True
+
+    if piece_has(cp, mp["up"]):
+        return True
+
+    if piece_has(cp, mp["right"]):
+        return True
+
+    if piece_has(cp, mp["down"]):
+        return True
+
+    return False
+
+def check_permutation(b):
+    """ Make a first pass at a specific permutation, without verifying
+        every single connection.
+        For corner spots, make sure they have at least two matches, one
+        for each ajoining piece
+        For side center pieces, make sure their are three matches, one
+        for each ajoining piece.
+        For the center, each side needs one match per side.
+
+        We can do even better, but this is a start."""
+
+    # For the middle, we need 4 matches
+    found = 0
+    cp = b["top"]["m"]
+    mp = b["middle"]["m"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    cp = b["middle"]["r"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    cp = b["middle"]["l"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    cp = b["bottom"]["m"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    if found != 4:
+        return False
+
+    if found > 4:
+        print "What, more than 4????"
+        print mp
+        print cp
+        print b
+        sys.exit(1)
+
+    # Top left compare
+    found = 0
+    mp = b["top"]["l"]
+    cp = b["top"]["m"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    cp = b["middle"]["l"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    if found == 2:
+        return True
+
+    # top right compare
+    found = 0
+    mp = b["top"]["r"]
+    p = b["top"]["m"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    cp = b["middle"]["r"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    if found == 2:
+        return True
+
+    # bottom left compare
+    found = 0
+    mp = b["bottom"]["l"]
+    cp = b["bottom"]["m"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    cp = b["middle"]["l"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    if found == 2:
+        return True
+
+    # bottom right compare
+    found = 0
+    mp = b["bottom"]["r"]
+    cp = b["bottom"]["m"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    cp = b["middle"]["r"]
+    if piece_match_piece(cp, mp):
+        found = found + 1
+
+    if found == 2:
+        return True
+
+    return False
 
 # With 3x3, make this row/column based
 def check_board(b):
-    """ See if this board order is valid """
+    """ See if this board order is a solved puzzle """
     if b["top"]["r"]["right"]["bird"] != b["top"]["m"]["left"]["bird"]:
         return False
     if b["top"]["r"]["right"]["side"] == b["top"]["m"]["left"]["side"]:
@@ -182,31 +320,12 @@ def check_board(b):
     return True
 
 
-if __name__ == "__main__":
-    """ Take our puzzle, compute permutations and start rotating """
+def brute_force(args, board): 
     found = 0
     perms = 0;
     tc = 0;
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--start", \
-                        help="Starting permutation count", type=int)
-    parser.add_argument("-q", "--quit", \
-                        help="quitting permutation count", type=int)
-    args = parser.parse_args()
-
-    for piece in pieces:
-        print_piece(piece, pieces[piece])
-
-    board["top"]["r"] = empty
-    board["top"]["m"] = empty
-    board["top"]["l"] = empty
-    board["middle"]["r"] = empty
-    board["middle"]["m"] = empty
-    board["middle"]["l"] = empty
-    board["bottom"]["r"] = empty
-    board["bottom"]["m"] = empty
-    board["bottom"]["l"] = empty
+    possible = 0;
+    skip = 0;
 
     for bp in itertools.permutations((x for x in range(1, 10)), 9):
         perms = perms + 1
@@ -220,19 +339,19 @@ if __name__ == "__main__":
     count = 0
     for bp in itertools.permutations((x for x in range(1, 10)), 9):
         count = count + 1
-        # Make command line option to start from a specific spot
         if (args.start and count < args.start):
             tc = tc + rotations
             continue
 
         if (args.quit and count > args.quit):
-	    break
+            break
 
         # print bp, " count:", count, " remaining:", perms - count
-	info_str = "\r" + str(bp) + " count:" + str(count) + " remaining:" + \
-	    str(perms - count) + "   "
-	sys.stdout.write(info_str)
-	sys.stdout.flush()
+        info_str = "\r" + str(bp) + " count:" + str(count) + " remaining:" + \
+            str(perms - count) + " skip:" + str(skip) + " possible:" + \
+            str(possible) + "    "
+        sys.stdout.write(info_str)
+        sys.stdout.flush()
         board["top"]["r"] = pieces[bp[0]]
         board["top"]["m"] = pieces[bp[1]]
         board["top"]["l"] = pieces[bp[2]]
@@ -243,14 +362,26 @@ if __name__ == "__main__":
         board["bottom"]["m"] = pieces[bp[7]]
         board["bottom"]["l"] = pieces[bp[8]]
 
+        # This is not really saving us much, as the check test is not
+        # very good.
+        if not check_permutation(board):
+            tc = tc + rotations
+            skip = skip + 1
+            continue
+
+        possible = possible + 1
+        #
+        # Another idea is to test rotated item right away for its edges
+        # and see if it passes.  Will this help us with speeding up the
+        # check board function?
         for x in range(0, rotations):
             if check_board(board):
-		found = found + 1
+                found = found + 1
                 print ""
                 print "found match", found, "at ", bp
-		print "after ", count, " permutaions and ", tc, " rotations"
-		print_board(board)
-		print ""
+                print "after ", count, " permutaions and ", tc, " rotations"
+                print_board(board)
+                print ""
 
             rotate_left(bp[8], pieces)
             tc = tc + 1
@@ -273,3 +404,28 @@ if __name__ == "__main__":
 
     print "\n", found, "matches after ", count, " permutaions and ", tc,\
       " rotations"
+
+
+if __name__ == "__main__":
+    """ Take our puzzle, compute permutations and start rotating """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--start", \
+                        help="Starting permutation count", type=int)
+    parser.add_argument("-q", "--quit", \
+                        help="quitting permutation count", type=int)
+    args = parser.parse_args()
+
+    for piece in pieces:
+        print_piece(pieces[piece])
+
+    board["top"]["r"] = empty
+    board["top"]["m"] = empty
+    board["top"]["l"] = empty
+    board["middle"]["r"] = empty
+    board["middle"]["m"] = empty
+    board["middle"]["l"] = empty
+    board["bottom"]["r"] = empty
+    board["bottom"]["m"] = empty
+    board["bottom"]["l"] = empty
+
+    brute_force(args, board)
